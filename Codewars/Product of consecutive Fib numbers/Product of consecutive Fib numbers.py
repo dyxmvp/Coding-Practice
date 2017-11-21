d583b242bb07025d72c0e50cc86429db996ae5d5
DYX <dyxmvp@gmail.com>
2017-11-20 19:09:24 -0500

    Product of consecutive Fib numbers Code

--- Codewars/Product of consecutive Fib numbers/Product of consecutive Fib numbers.py
+++ Codewars/Product of consecutive Fib numbers/Product of consecutive Fib numbers.py
@@ -1,10 +1,6 @@
-def race(v1, v2, g):
-    v_diff = v2 - v1
-    if v_diff <= 0:
-        return None
-    else:
-        t = g / v_diff
-        t_h = int(t)
-        t_min = int(round((t - t_h) * 60, 10))
-        t_s = int(((t - t_h) * 60 - t_min) * 60)
-        return [t_h, t_min, t_s]
+def productFib(prod):
+    # your code
+    F0, F1 = 0, 1
+    while F0 * F1 < prod:
+        F0, F1 = F1, F0 + F1
+    return [F0, F1, F0 * F1 == prod]
